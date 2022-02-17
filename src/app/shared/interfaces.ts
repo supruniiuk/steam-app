d27cec0025f7773a03f512f049a297da21737d65
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-02-17 16:31:05 +0200

    added interfaces

---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,24 @@
+export interface LoginInfo {
+    email: string;
+    password: string;
+    returnSecureToken?: boolean;
+  }
+  
+  export interface FirebaseToken {
+    idToken: string;
+    expiresIn: string;
+    localId?: string;
+  }
+  
+  export interface User {
+      id?: string;
+      email: string;
+      username: string;
+      age: number | null;
+      friendsList: string[];
+      gamesList: string[];
+  }
+  
+  export interface ResponseName {
+      name: string;
+  }
