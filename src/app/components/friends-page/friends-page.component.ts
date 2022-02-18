a65c664d6d9951a32d4cea499b2007e1aa3d4a88
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-02-18 21:01:01 +0200

    friends adding and removing

--- src/app/components/friends-page/friends-page.component.ts
+++ src/app/components/friends-page/friends-page.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Friend, User } from 'src/app/shared/interfaces';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-friends-page',
   templateUrl: './friends-page.component.html',
-  styleUrls: ['./friends-page.component.scss']
+  styleUrls: ['./friends-page.component.scss'],
 })
 export class FriendsPageComponent implements OnInit {
+  users: User[] = [];
+  errorMessage: string = '';
+  srcStr: string = '';
+  user: User;
+  friendsList: Friend[];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    const userInfo: string | null = localStorage.getItem('userInfo');
+    this.user = JSON.parse(userInfo ? userInfo : '');
+    if (this.user.friendsList && this.user.friendsList.length > 0) {
+      this.friendsList = this.user.friendsList;
+    } else {
+      this.friendsList = [];
+    }
+
+    this.userService.getAllUsers().subscribe(
+      (res) => {
+        this.users = Object.keys(res).map((key: any) => {
+          res[key].id = key;
+          return res[key];
+        });
+
+        this.users = this.filterUsers();
+      },
+      (err) => (this.errorMessage = err.message)
+    );
   }
 
+  filterUsers() {
+    const friends = this.friendsList.map((f) => f.id);
+
+    return this.users.filter(
+      (u) => u.email !== this.user.email && !friends.includes(u.id)
+    );
+  }
+
+  remove(exFriend: Friend) {
+    this.friendsList = this.friendsList.filter((f) => f.id != exFriend.id);
+  }
+
+  add(newFriend: Friend) {
+    this.friendsList.unshift(newFriend);
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,25 +1,30 @@
 export interface LoginInfo {
-    email: string;
-    password: string;
-    returnSecureToken?: boolean;
-  }
-  
-  export interface FirebaseToken {
-    idToken: string;
-    expiresIn: string;
-    localId?: string;
-    email: string;
-  }
-  
-  export interface User {
-      id?: string;
-      email: string;
-      username: string;
-      age: number | null;
-      friendsList: string[];
-      gamesList: string[];
-  }
-  
-  export interface ResponseName {
-      name: string;
-  }
+  email: string;
+  password: string;
+  returnSecureToken?: boolean;
+}
+
+export interface FirebaseToken {
+  idToken: string;
+  expiresIn: string;
+  localId?: string;
+  email: string;
+}
+
+export interface User {
+  id?: string;
+  email: string;
+  username: string;
+  age: number | null;
+  friendsList: Friend[];
+  gamesList: string[];
+}
+
+export interface ResponseName {
+  name: string;
+}
+
+export interface Friend {
+  email: string;
+  id: string;
+}
