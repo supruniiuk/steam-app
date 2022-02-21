8565a09a2aabc1ef24c5f9b7cc8bd9f2f4f3b2b1
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-02-21 22:26:14 +0200

    added tests for friends

--- src/app/components/friends-page/friends-page.component.spec.ts
+++ src/app/components/friends-page/friends-page.component.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { GameService } from 'src/app/shared/services/games.service';
+import { EMPTY, of } from 'rxjs';
+import { User } from 'src/app/shared/interfaces';
 import { UserService } from 'src/app/shared/services/user.service';
 
 import { FriendsPageComponent } from './friends-page.component';
@@ -10,6 +11,17 @@ import { FriendsPageComponent } from './friends-page.component';
 describe('FriendsPageComponent', () => {
   let component: FriendsPageComponent;
   let userService: UserService;
+  let info: User = {
+    age: 50,
+    email: 'exmple@gmail.com',
+    friendsList: [
+      { email: 'bcd@gmail.com', id: '-MwDEvoJA0MuEMAwUPLu' },
+      { email: 'ijkl@gmail.com', id: '-MwDFYPchUDxdD9rcHze' },
+    ],
+    gamesList: [{ id: 'id1' }],
+    id: '-MwDFeGzNW8gbGRQicfQ',
+    username: 'test',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,9 +35,45 @@ describe('FriendsPageComponent', () => {
 
     userService = TestBed.inject(UserService);
     component = new FriendsPageComponent(userService);
+
+    spyOn(userService, 'getCurrentUserInfo').and.callFake(() => {
+      return info;
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('calls getAllUsers when ngOnInit', () => {
+    const spy = spyOn(userService, 'getAllUsers').and.callFake(() => {
+      return EMPTY;
+    });
+
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('remove friend', () => {
+    let exFriend = { id: '-MwDErAZp7iK98goW2PU', email: 'abc@gmail.com' };
+    let friends = [
+      { email: 'bcd@gmail.com', id: '-MwDEvoJA0MuEMAwUPLu' },
+      { email: 'ijkl@gmail.com', id: '-MwDFYPchUDxdD9rcHze' },
+    ];
+    component.ngOnInit()
+    component.remove(exFriend);
+    expect(component.friendsList.length).toEqual(friends.length);
+  });
+
+  it('add friend', () => {
+    let newFriend = { id: '-MwDErAZp7iK98goW2PU', email: 'abc@gmail.com' };
+    let friends = [
+      { id: '-MwDErAZp7iK98goW2PU', email: 'abc@gmail.com' },
+      { email: 'bcd@gmail.com', id: '-MwDEvoJA0MuEMAwUPLu' },
+      { email: 'ijkl@gmail.com', id: '-MwDFYPchUDxdD9rcHze' },
+    ];
+    component.ngOnInit()
+    component.add(newFriend);
+    expect(component.friendsList.length).toEqual(friends.length);
   });
 });
