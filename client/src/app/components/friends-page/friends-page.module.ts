13875b68cc77e7e1f74e27d81bb798c26a4e092a
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-04-08 22:07:55 +0300

    added lazy-loading

---
+++ client/src/app/components/friends-page/friends-page.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FriendItemComponent } from './friend-item/friend-item.component';
+import { PipesModule } from 'src/app/shared/modules/pipes.module';
+import { FriendsPageComponent } from './friends-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PipesModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: FriendsPageComponent,
+      },
+    ]),
+  ],
+  declarations: [FriendsPageComponent, FriendItemComponent],
+})
+export class FriendsPageModule { }
