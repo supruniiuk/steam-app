e1daf667a0157cc9664ff5a787eb9c28f03eb301
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-04-21 16:25:51 +0300

    updated friends actions

--- server/controllers/friendController.js
+++ server/controllers/friendController.js
@@ -4,20 +4,8 @@ const Friend = require("../models/friend");
 const User = require("../models/user");
 
 class FriendController {
-  async getPossibleFriends(req, res, next) {
-    const userId = req.user.id;
-
-
-    try {
-    const friends = await Friend.find({
-      $or: [{ publisherId: userId }, { subscriberId: userId }],
-    });
-
-    const friendsIdArr = friends.map(friend => friend._id).concat(userId);
-
-    let possibleFriends = await User.find({ _id: { $nin: friendsIdArr },  role: { $nin: 'admin' }}, {password: 0, updatedAt: 0, __v: 0})
-
-    possibleFriends = possibleFriends.map(friend => {
+  static getFriendsArray(friends) {
+    return friends.map((friend) => {
       return {
         id: friend._id,
         username: friend.username,
@@ -25,29 +13,119 @@ class FriendController {
         role: friend.role,
         birthday: friend.birthday,
         createdAt: friend.createdAt,
+      };
+    });
+  }
+
+  static getFriendsId(connections, userId) {
+    return connections.map((friend) => {
+      if (friend.publisherId.toString() === userId) {
+        return friend.subscriberId.toString();
+      } else {
+        return friend.publisherId.toString();
       }
-    })
-    
-    res.json(possibleFriends);
-  } catch (err) {
-      return next(ApiError.internal(`Server error`));
-    }
+    });
   }
 
   async getFriends(req, res, next) {
     const userId = req.user.id;
 
     try {
-      const friends = await Friend.find({
-        $or: [{ publisherId: userId }, { subscriberId: userId }],
+      const connections = await Friend.find({
+        $or: [
+          { publisherId: userId, approved: true },
+          { subscriberId: userId, approved: true },
+        ],
       });
+
+      const friendsId = FriendController.getFriendsId(connections, userId);
+
+      let friends = await User.find(
+        { _id: { $in: friendsId } },
+        { password: 0, updatedAt: 0, __v: 0 }
+      );
+      friends = FriendController.getFriendsArray(friends);
+
       res.json(friends);
     } catch (err) {
       return next(ApiError.internal(`Server error`));
     }
   }
 
-  async addFriend(req, res, next) {
+  async getSubscriptions(req, res, next) {
+    const userId = req.user.id;
+
+    try {
+      const connections = await Friend.find({
+        subscriberId: userId,
+        approved: false,
+      });
+
+      const publishersId = FriendController.getFriendsId(connections, userId);
+
+      let subscriptions = await User.find(
+        { _id: { $in: publishersId } },
+        { password: 0, updatedAt: 0, __v: 0 }
+      );
+
+      subscriptions = FriendController.getFriendsArray(subscriptions);
+
+      res.json(subscriptions);
+    } catch (err) {
+      return next(ApiError.internal(`Server error`));
+    }
+  }
+
+  async getPossibleFriends(req, res, next) {
+    const userId = req.user.id;
+
+    try {
+      const friends = await Friend.find({
+        $or: [{ publisherId: userId }, { subscriberId: userId }],
+      });
+
+      const friendsIdArr = FriendController.getFriendsId(
+        friends,
+        userId
+      ).concat(userId);
+
+      let possibleFriends = await User.find(
+        { _id: { $nin: friendsIdArr }, role: { $nin: "admin" } },
+        { password: 0, updatedAt: 0, __v: 0 }
+      );
+
+      possibleFriends = FriendController.getFriendsArray(possibleFriends);
+
+      res.json(possibleFriends);
+    } catch (err) {
+      return next(ApiError.internal(`Server error`));
+    }
+  }
+
+  async getFriendRequests(req, res, next) {
+    const userId = req.user.id;
+
+    try {
+      const requests = await Friend.find({
+        publisherId: userId,
+        approved: false,
+      });
+
+      const subscribersIdArr = FriendController.getFriendsId(requests, userId);
+
+      let subscribers = await User.find(
+        { _id: { $in: subscribersIdArr } },
+        { password: 0, updatedAt: 0, __v: 0 }
+      );
+
+      subscribers = FriendController.getFriendsArray(subscribers);
+      res.json(subscribers);
+    } catch (err) {
+      return next(ApiError.internal(`Server error`));
+    }
+  }
+
+  async addNewFriend(req, res, next) {
     const publisherId = req.body.publisherId;
     const userId = req.user.id;
 
@@ -56,6 +134,17 @@ class FriendController {
     }
 
     try {
+      const isExist = await Friend.find({
+        $or: [
+          { publisherId: userId, subscriberId: publisherId },
+          { publisherId: publisherId, subscriberId: userId },
+        ],
+      });
+
+      if (isExist.length) {
+        return next(ApiError.badRequest(`Friend request already sent`));
+      }
+
       const friend = new Friend({ subscriberId: userId, publisherId });
       friend.save();
       res.json({ message: `Friend's request successfully created` });
@@ -66,18 +155,15 @@ class FriendController {
 
   async approveFriend(req, res, next) {
     const userId = req.user.id;
-    const requestId = req.params.id;
-
-    if (!mongoose.isValidObjectId(requestId)) {
-      return next(ApiError.badRequest(`Invalid friend request id`));
-    }
+    const friendId = req.params.id;
 
     try {
-      const request = await Friend.findOne({ _id: requestId });
-      if (userId !== request.publisherId.toString()) {
-        return next(ApiError.badRequest(`You can't approve request`));
-      }
-      await Friend.findByIdAndUpdate({ _id: requestId }, { approved: true });
+      const request = await Friend.findOne({
+        publisherId: userId,
+        subscriberId: friendId,
+      });
+
+      await Friend.findByIdAndUpdate({ _id: request._id }, { approved: true });
       res.json({ message: `Friend's request successfully approved` });
     } catch (err) {
       return next(ApiError.internal(`Server error`));
@@ -85,15 +171,18 @@ class FriendController {
   }
 
   async deleteFriend(req, res, next) {
-    const requestId = req.params.id;
-
-    if (!mongoose.isValidObjectId(requestId)) {
-      return next(ApiError.badRequest(`Invalid friend request id`));
-    }
+    const userId = req.user.id;
+    const friendId = req.params.id;
 
     try {
-      await Friend.findByIdAndDelete({ _id: requestId });
-      res.json({ message: "Friend successfully deleted" });
+      await Friend.findOneAndDelete({
+        $or: [
+          { publisherId: userId, subscriberId: friendId },
+          { publisherId: friendId, subscriberId: userId },
+        ],
+      });
+
+      res.json({ message: "Friend request successfully canceled" });
     } catch (err) {
       return next(ApiError.internal(`Server error`));
     }
--- server/routes/friends.js
+++ server/routes/friends.js
@@ -1,12 +1,16 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
 
-const authMiddleware = require('../middleware/authMiddleware');
-const FriendController = require('../controllers/friendController');
+const authMiddleware = require("../middleware/authMiddleware");
+const FriendController = require("../controllers/friendController");
 
-router.get('/', authMiddleware, FriendController.getPossibleFriends);
-router.post('/', authMiddleware, FriendController.addFriend);
-router.patch('/:id', authMiddleware, FriendController.approveFriend);
-router.delete('/:id', authMiddleware, FriendController.deleteFriend);
+router.get("/", authMiddleware, FriendController.getFriends);
+router.get("/search", authMiddleware, FriendController.getPossibleFriends);
+router.get("/subs", authMiddleware, FriendController.getSubscriptions);
+router.get("/new", authMiddleware, FriendController.getFriendRequests);
+
+router.post("/", authMiddleware, FriendController.addNewFriend);
+router.patch("/:id", authMiddleware, FriendController.approveFriend);
+router.delete("/:id", authMiddleware, FriendController.deleteFriend);
 
 module.exports = router;
